1dd6cc2f67068bc72e9f53c4c8a759cf37312e57
Talia Seada <78349342+TaliaSeada@users.noreply.github.com>
2024-06-19 18:33:37 +0300

    need to do:

--- primeCounter.c
+++ primeCounter.c
@@ -4,86 +4,80 @@
 #include <stdatomic.h>
 #include <math.h>
 #include <stdint.h>
+#include <unistd.h>
 
-#define MAX_ARRAY_SIZE 500000
-
-int array[MAX_ARRAY_SIZE];
-atomic_int shared_counter;
-int total_primes = 0;
-pthread_mutex_t total_primes_lock;
+// Shared data
+atomic_int total_counter = ATOMIC_VAR_INIT(0);
+pthread_mutex_t counter_mutex = PTHREAD_MUTEX_INITIALIZER;
 
 // Function to check if a number is prime
 bool isPrime(int num)
 {
-    if (num <= 1) return false;
-    if (num <= 3) return true;
-    if (num % 2 == 0 || num % 3 == 0) return false;
+    if (num <= 1)
+        return false;
+    if (num <= 3)
+        return true;
+    if (num % 2 == 0 || num % 3 == 0)
+        return false;
     int limit = sqrt(num);
     for (int i = 5; i <= limit; i += 6)
     {
-        if (num % i == 0 || num % (i + 2) == 0) return false;
+        if (num % i == 0 || num % (i + 2) == 0)
+            return false;
     }
     return true;
 }
 
-// Worker function to find prime numbers in the array
-void *worker(void *arg)
+// Function for each thread to process numbers
+void *thread_function(void *arg)
 {
-    while (true)
+    int thread_id = *(int *)arg;
+    int local_counter = 0;
+    int num;
+
+    // Read numbers from stdin in chunks
+    while (scanf("%d", &num) != EOF)
     {
-        int start = atomic_fetch_sub(&shared_counter, 1000);
-        if (start <= 0) break;
-
-        int end = (start - 1000 < 0) ? 0 : start - 1000;
-        int local_counter = 0;
-
-        for (int index = start - 1; index >= end; index--)
+        // Check if the number is prime
+        if (isPrime(num))
         {
-            if (isPrime(array[index]))
-            {
-                local_counter++;
-            }
+            local_counter++;
         }
-
-        pthread_mutex_lock(&total_primes_lock);
-        total_primes += local_counter;
-        pthread_mutex_unlock(&total_primes_lock);
     }
+
+    // Atomically update the global counter
+    pthread_mutex_lock(&counter_mutex);
+    total_counter += local_counter;
+    pthread_mutex_unlock(&counter_mutex);
+
     return NULL;
 }
 
 int main()
 {
-    int num;
-    pthread_t threads[4];
+    int num_threads;
+    // Determine number of threads to use
+    num_threads = sysconf(_SC_NPROCESSORS_ONLN);
 
-    // Initialize mutex
-    pthread_mutex_init(&total_primes_lock, NULL);
+    pthread_t threads[num_threads];
+    int thread_ids[num_threads];
+    int i;
 
-    // Read numbers from stdin and store them in the array until end of file or array full
-    int array_size = 0;
-    while (scanf("%d", &num) != EOF && array_size < MAX_ARRAY_SIZE)
+    // Create threads
+    for (i = 0; i < num_threads; i++)
     {
-        array[array_size++] = num;
-    }
-    atomic_store(&shared_counter, array_size);
-
-    // Create 4 worker threads
-    for (int i = 0; i < 4; i++)
-    {
-        pthread_create(&threads[i], NULL, worker, NULL);
+        thread_ids[i] = i;
+        pthread_create(&threads[i], NULL, thread_function, &thread_ids[i]);
     }
 
-    // Wait for all threads to complete
-    for (int i = 0; i < 4; i++)
+    // Join threads
+    for (i = 0; i < num_threads; i++)
     {
         pthread_join(threads[i], NULL);
     }
 
-    printf("%d total primes.\n", total_primes);
-
-    // Destroy mutex
-    pthread_mutex_destroy(&total_primes_lock);
+    // Print the total number of prime numbers found
+    printf("%d total primes.\n", total_counter);
 
     return 0;
 }
