c7ee86ee10fcbdd93b7f14b54fc4d60196e71281
Talia Seada <78349342+TaliaSeada@users.noreply.github.com>
2024-06-23 16:48:17 +0300

    images

--- primeCounter.c
+++ primeCounter.c
@@ -10,6 +10,19 @@
 atomic_int total_counter = ATOMIC_VAR_INIT(0);
 pthread_mutex_t counter_mutex = PTHREAD_MUTEX_INITIALIZER;
 
+// // Function to check if a number is prime
+// bool isPrime(int n) {
+//     if (n <= 1) {
+//         return false;
+//     }
+//     for (int i = 2; i * i <= n; i++) {
+//         if (n % i == 0) {
+//             return false;
+//         }
+//     }
+//     return true;
+// }
+
 // Function to check if a number is prime
 bool isPrime(int num)
 {
@@ -27,6 +40,22 @@ bool isPrime(int num)
     }
     return true;
 }
+
+// int main() {
+//     int num;
+//     int total_counter = 0;
+
+//     // Read numbers from stdin until end of file
+//     while (scanf("%d", &num) != EOF) {   
+//         if (isPrime(num)) {
+//             total_counter++;
+//         }
+//     }
+
+//     printf("%d total primes.\n", total_counter);
+
+//     return 0;
+// }
 
 // Function for each thread to process numbers
 void *thread_function(void *arg)
